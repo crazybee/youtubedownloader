1685304527f057c7c1700fd1716dc4c52644f735
Zhe Liu <zhe.liu@nl.abb.com>
2020-01-01 22:59:33 +0100

    firstpush

---
+++ Downloader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YoutubeVideoDownloader
+{
+    public class Downloader :IDownloader
+    {
+        private string url;
+        private bool audioOnly;
+
+        public Downloader(string url, bool audioOnly)
+        {
+            this.url = url;
+            this.audioOnly = audioOnly;
+        }
+
+        public async Task<bool> Download()
+        {
+            var id = YoutubeClient.ParseVideoId(this.url);
+
+            var client = new YoutubeClient();
+
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
+            MediaStreamInfo streamInfo;
+            if (!audioOnly)
+            {
+                streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
+            }
+            else
+            {
+                streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+            }
+
+          
+
+            var ext = streamInfo.Container.GetFileExtension();
+            try
+            {
+                if (!this.audioOnly)
+                {
+
+                    await client.DownloadMediaStreamAsync(streamInfo,
+                        $"VideoDownloaded{DateTime.UtcNow:yyyyMMDDhhmm}.{ext}", this.Displayprogress());
+                }
+                else
+                {
+                    await client.DownloadMediaStreamAsync(streamInfo, $"AudioDownloaded{DateTime.UtcNow:yyyyMMDDhhmm}.{ext}", this.Displayprogress());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        private IProgress<double> Displayprogress()
+        {
+           return new Progress<double>(p =>
+           {
+               Console.SetCursorPosition(0, 6);
+               Console.Write(Convert.ToInt16(p*100).ToString() + "% \r");
+           });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YoutubeVideoDownloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+
+        {
+             string url;
+            do
+            {
+                Console.WriteLine("paste youtube url here");
+                url = Console.ReadLine();
+              
+            } while (url == string.Empty);
+
+            string audioOnly;
+            do
+            {
+                Console.WriteLine("only extract audio?(y/n)");
+                audioOnly = Console.ReadLine();
+
+            } while (audioOnly != "y" && audioOnly != "n");
+
+            var downloader = new Downloader(url, string.Equals(audioOnly, "y", StringComparison.InvariantCultureIgnoreCase));
+            Console.WriteLine("downloading...");
+            var downloadResult = await downloader.Download();
+            Console.WriteLine(downloadResult ? "Downloaded" : "Failed");
+        }
+    }
+}
